6f072a2f861514ffeac0e10ae474d3cb8e79b314
Benjamin Steeper <bds238@cornell.edu>
2022-04-19 19:24:07 -0400

    new repo for project

---
+++ src/main/java/com/steeper/ben/GitVersionMojo.java
@@ -0,0 +1,21 @@
+package com.steeper.ben;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "helloworld", defaultPhase = LifecyclePhase.INITIALIZE)
+public class GitVersionMojo extends AbstractMojo {
+
+    @Parameter(property = "project", readonly = true)
+    private MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("hello world, this is my maven plugin!");
+    }
+}
