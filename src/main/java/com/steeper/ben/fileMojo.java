3e8f8606dd9efa6e2fa425b231532c605a22c9cc
Benjamin Steeper <bds238@cornell.edu>
2022-04-22 02:17:12 -0400

    added JarWork class with a method to extract jar files

--- src/main/java/com/steeper/ben/fileMojo.java
+++ src/main/java/com/steeper/ben/fileMojo.java
@@ -10,6 +10,7 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.IOException;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.util.List;
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ public class fileMojo extends AbstractMojo {
     @Parameter(property = "txtFilePath", defaultValue = "/../rv_project/test.txt")
     private String txtFilePath;
 
-    @Parameter(property = "agentJarPath", defaultValue = "/../agents/JavaMOPAgent.jar")
+    @Parameter(property = "agentJarPath", defaultValue = "/../agents")
     private String agentJarPath;
 
     @Override
@@ -40,6 +41,50 @@ public class fileMojo extends AbstractMojo {
         }
         getLog().info(agentJarPath);
         getLog().info(txtFilePath);
+
+        // Create JarWork class to start working with Jar
+        JarWork agentJar = new JarWork();
+        // Try extracting the agent jar file
+        try {
+            agentJar.ExtractJar();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class JarWork {
+
+        public void main(String[] args) throws java.io.IOException {
+            getLog().info("New JarWork class created...");
+        }
+
+        public void ExtractJar() throws java.io.IOException {
+            getLog().info("Extracting Jar...");
+            java.util.jar.JarFile jarfile = new java.util.jar.JarFile(new java.io.File(agentJarPath + "/JavaMOPAgent.jar")); //jar file path(here sqljdbc4.jar)
+            java.util.Enumeration<java.util.jar.JarEntry> enu = jarfile.entries();
+            while (enu.hasMoreElements()) {
+                String destdir = agentJarPath;    // destination directory
+                java.util.jar.JarEntry je = enu.nextElement();
+
+                System.out.println(je.getName());
+
+                java.io.File fl = new java.io.File(destdir, je.getName());
+                if (!fl.exists()) {
+                    fl.getParentFile().mkdirs();
+                    fl = new java.io.File(destdir, je.getName());
+                }
+                if (je.isDirectory()) {
+                    continue;
+                }
+                java.io.InputStream is = jarfile.getInputStream(je);
+                java.io.FileOutputStream fo = new java.io.FileOutputStream(fl);
+                while (is.available() > 0) {
+                    fo.write(is.read());
+                }
+                fo.close();
+                is.close();
+            }
+        }
     }
 
     public class ReadFile {
