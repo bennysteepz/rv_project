a44b907e23c2a5ead991254b63ab71b9c8a94f19
Benjamin Steeper <bds238@cornell.edu>
2022-05-15 20:28:45 -0400

    added testStartsMojo

---
+++ src/main/java/com/steeper/ben/testStartsMojo.java
@@ -0,0 +1,79 @@
+package com.steeper.ben;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+
+import java.io.*;
+import java.util.List;
+import java.util.jar.*;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Collections;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Mojo(name = "rebuildAgent", requiresDependencyResolution = ResolutionScope.TEST)
+public class testStartsMojo extends AbstractMojo {
+
+    @Parameter(property = "project", readonly = false)
+    private MavenProject project;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Starting testStarts execute() method...");
+
+        String clientPomPath = "pom.xml"; // get this programmatically later, for now it's just hard coded
+
+//        invokeMaven(clientPomPath, "starts:run");
+    }
+
+    private void invokeMaven(String pomPath, String command) {
+
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(new File(pomPath));
+        request.setGoals(Collections.singletonList(command));
+
+        Invoker invoker = new DefaultInvoker();
+//            invoker.setMavenHome();
+
+        try {
+            getLog().info("Executing Maven invoker request...");
+            invoker.execute(request);
+        }
+        catch (MavenInvocationException e) {
+            e.printStackTrace();
+        }
+    }
+}
